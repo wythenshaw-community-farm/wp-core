54059087ac1f52f896b7a935c351a8c18d497b4d
Toby Griffiths <toby@cubicmushroom.co.uk>
2017-09-18 20:01:58 +0100

    Refactored plugin code (non-functional change)

--- web/app/mu-plugins/wcf-animals/post-types.php
+++ web/app/mu-plugins/wcf-animals/post-types.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Plugin Name: WCF Animals
- * Description: Provides functionality relating to the animals on the farm
- * Version: 1.0.0
- * Author: Toby Griffiths <toby@cubicmushroom.co.uk>
- * Author URI: http://blog.tobyg.net/
- * License: MIT License
- */
 
 namespace WCF\Animals;
 
